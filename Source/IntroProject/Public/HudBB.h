9e731368b04f1bda7c0fac8b8b75fb5796fa2071
Echo Yin <echo_ybl@hotmail.com>
2023-12-12 20:49:58 -0500

    Inherent from different classes

---
+++ Source/IntroProject/Public/HudBB.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HudBB.generated.h"
+
+UCLASS()
+class INTROPROJECT_API AHudBB : public AHUD
+{
+	GENERATED_BODY()
+
+};
---
+++ Source/IntroProject/Public/PlayerControllerBB.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerControllerBB.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTROPROJECT_API APlayerControllerBB : public APlayerController
+{
+	GENERATED_BODY()
+};
