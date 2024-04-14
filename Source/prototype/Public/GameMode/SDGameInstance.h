243efd7cd34fa0ad3e6dc19bb91705a41b7798ff
Echo Yin <echo_ybl@hotmail.com>
2024-04-14 15:36:46 -0400

    Add basic setups for shadow dungeon

---
+++ Source/prototype/Public/GameMode/SDGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SDGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE_API USDGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/prototype/Public/GameMode/SDGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "SDGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE_API ASDGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
