0fa290b24b834be60536a81f4ae904bf0ee43f4f
Echo Yin <echo_ybl@hotmail.com>
2024-04-14 18:03:37 -0400

    Add controls to player character

--- Source/prototype/Public/GameMode/SDCharacter.h
+++ Source/prototype/Public/GameMode/SDCharacter.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "SDCharacter.generated.h"
 
@@ -15,9 +16,45 @@ public:
 	// Sets default values for this character's properties
 	ASDCharacter();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class UCameraComponent> FollowCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UInputAction> SprintAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UInputAction> InteractActin;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	// Move
+	void Move(const struct FInputActionValue& Value);
+
+	// Look
+	void Look(const FInputActionValue& Value);
+
+	// Sprint Start
+	void SprintStart(const FInputActionValue& Value);
+
+	// Sprint End
+	void SprintEnd(const FInputActionValue& Value);
+
+	// Interact
+	void Interact(const FInputActionValue& Value);
 
 public:	
 	// Called every frame
@@ -26,4 +63,14 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Movement")
+	float MaxWalkSpeed = 500.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Movement")
+	float MaxSprintSpeed = 3000.0f;
+
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
