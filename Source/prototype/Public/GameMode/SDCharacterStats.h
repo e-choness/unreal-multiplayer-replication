f8f67d569a79afdd78213dfb2306a386dd0e3249
Echo Yin <echo_ybl@hotmail.com>
2024-04-14 22:03:22 -0400

    Create a data table row for passing information around

---
+++ Source/prototype/Public/GameMode/SDCharacterStats.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "SDCharacterStats.generated.h"
+
+USTRUCT(BlueprintType)
+struct PROTOTYPE_API FSDCharacterStats : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+	float WalkSpeed = 200.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+	float SprintSpeed = 400.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+	float DamageMultiplier = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damange")
+	float StealthMultiplier = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="XP")
+	int32 NextLevelXp = 10;
+	
+};
