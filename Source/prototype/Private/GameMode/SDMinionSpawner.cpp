2036d914c1e90d46c0062bfcae8660c107aac5ad
Echo Yin <echo_ybl@hotmail.com>
2024-05-03 20:14:31 -0600

    Add spawn logic at begin play

--- Source/prototype/Private/GameMode/SDMinionSpawner.cpp
+++ Source/prototype/Private/GameMode/SDMinionSpawner.cpp
@@ -2,6 +2,8 @@
 
 
 #include "GameMode/SDMinionSpawner.h"
+#include "GameMode/SDMinion.h"
+#include "Components/BoxComponent.h"
 
 // Sets default values
 ASDMinionSpawner::ASDMinionSpawner()
@@ -9,13 +11,44 @@ ASDMinionSpawner::ASDMinionSpawner()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpawnArea = CreateDefaultSubobject<UBoxComponent>(TEXT("Spawn Area"));
+	SpawnArea->SetupAttachment(RootComponent);
+	SpawnArea->SetBoxExtent(FVector(1000.0f, 1000.0f, 100.0f));
+
+	bReplicates = true;
 }
 
 // Called when the game starts or when spawned
 void ASDMinionSpawner::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if(SpawnableMinions.IsEmpty()) return;
+
+	if(GetLocalRole() != ROLE_Authority) return;
+
+	for(int32 i=0; i<MinionsAtStart; i++)
+	{
+		Spawn();
+	}
+
+	GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &ASDMinionSpawner::Spawn, SpawnDelay, true, SpawnDelay);
+}
+
+void ASDMinionSpawner::Spawn()
+{
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::DontSpawnIfColliding;
+
+	const auto Minion = SpawnableMinions[FMath::RandRange(0, SpawnableMinions.Num()-1)];
+
+	const auto Rotation = FRotator(0.0f, FMath::RandRange(0.0f, 360.0f), 0.0f);
 	
+	const auto SpawnX = SpawnArea->GetComponentLocation().X;
+	const auto SpawnY = SpawnArea->GetComponentLocation().Y;
+	const auto Location = SpawnArea->GetComponentLocation() + FVector(FMath::RandRange(-SpawnX, SpawnX), FMath::RandRange(-SpawnY, SpawnY), 0.0f);
+
+	GetWorld()->SpawnActor<ASDMinion>(Minion, Location, Rotation, SpawnParameters);
 }
 
 // Called every frame
--- Source/prototype/Public/GameMode/SDMinionSpawner.h
+++ Source/prototype/Public/GameMode/SDMinionSpawner.h
@@ -6,10 +6,28 @@
 #include "GameFramework/Actor.h"
 #include "SDMinionSpawner.generated.h"
 
+class ASDMinion;
+class UBoxComponent;
+
 UCLASS()
 class PROTOTYPE_API ASDMinionSpawner : public AActor
 {
 	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawn System", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UBoxComponent> SpawnArea;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn System", meta=(AllowPrivateAccess="true"))
+	TArray<TSubclassOf<ASDMinion>> SpawnableMinions;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn System", meta=(AllowPrivateAccess="true"))
+	float SpawnDelay = 10.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Spawn System", meta=(AllowPrivateAccess="true"))
+	int32 MinionsAtStart = 5;
+
+	UPROPERTY()
+	FTimerHandle SpawnTimerHandle;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -18,6 +36,9 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void Spawn();
 
 public:	
 	// Called every frame
