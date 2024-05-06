bf76cd1145c61f402b9f7775c3e03f557f72d5a7
Echo Yin <echo_ybl@hotmail.com>
2024-05-05 21:29:13 -0600

    Add weapon projectile component with input

---
+++ Source/prototype/Private/GameMode/SDWeaponProjectileComponent.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/SDWeaponProjectileComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameMode/SDWeaponProjectileBase.h"
+#include "GameMode/SDCharacter.h"
+
+// Sets default values for this component's properties
+USDWeaponProjectileComponent::USDWeaponProjectileComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+	ProjectileClass = ASDWeaponProjectileBase::StaticClass();
+}
+
+void USDWeaponProjectileComponent::SetProjectileClass(TSubclassOf<ASDWeaponProjectileBase> NewProjectileClass)
+{
+	ProjectileClass = NewProjectileClass;
+}
+
+
+// Called when the game starts
+void USDWeaponProjectileComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Add input action based on the weapon we are using, avoid clutter inputs to the player character
+	const ACharacter* Character = Cast<ACharacter>(GetOwner());
+	if(!Character) return;
+
+	if(const APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(WeaponMappingContext, 1);
+		}
+
+		if(UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		{
+			EnhancedInputComponent->BindAction(ThrowAction, ETriggerEvent::Triggered, this, &USDWeaponProjectileComponent::Throw);
+		}
+	}
+}
+
+void USDWeaponProjectileComponent::Throw()
+{
+	Throw_Server();
+}
+
+void USDWeaponProjectileComponent::Throw_Server_Implementation()
+{
+	if(!ProjectileClass) return;
+
+	const auto Character = Cast<ASDCharacter>(GetOwner());
+	const auto ProjectileSpawnLocation = GetComponentLocation();
+	const auto ProjectileSpawnRotation = GetComponentRotation();
+	
+	auto ProjectileParams = FActorSpawnParameters();
+	ProjectileParams.Owner = GetOwner();
+	ProjectileParams.Instigator = Character;
+
+	GetWorld()->SpawnActor<ASDWeaponProjectileBase>(ProjectileClass, ProjectileSpawnLocation, ProjectileSpawnRotation, ProjectileParams);
+}
+
+
+// Called every frame
+void USDWeaponProjectileComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
